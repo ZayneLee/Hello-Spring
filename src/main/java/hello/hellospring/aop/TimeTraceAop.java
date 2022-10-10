e028c75e8b2a4527283d3e24d95416c71263e7c9
Zayne <star1582@naver.com>
2022-10-10 22:09:55 +0900

    starter-aop

---
+++ src/main/java/hello/hellospring/aop/TimeTraceAop.java
@@ -0,0 +1,10 @@
+package hello.hellospring.aop;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+
+@Aspect
+public class TimeTraceAop {
+    
+}
